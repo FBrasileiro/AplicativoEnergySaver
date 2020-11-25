0c86abd6758cd499393426459708c658bdbc50c0
Felipe Brasileiro <felipe.tanajura@gmail.com>
2020-11-25 13:05:29 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding:50}}>
+     <Text>INICIANDO</Text>
+    </View>
+  );
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# App Energy Saver
+
+Aplicativo desenvolvido para o projeto da matéria Desafio Pesquisador no Senai CIMATEC
